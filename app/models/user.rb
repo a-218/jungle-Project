65cab683db3c86532b7dbd073f2e59a5e12ff61c
Alex Li <lialex0218@gmail.com>
2021-07-27 04:28:42 +0000

    implemented user login authentication tests

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,28 @@ class User < ActiveRecord::Base
 
   has_secure_password
   
+
+
+  validates :name, presence: true
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :email, :uniqueness => { :case_sensitive => false }, presence: true
+  #validates :password_digest, presence: true, length: { minimum: 6 }
+  validates :password, presence: true, length: { minimum: 5 }
+  validates :password_confirmation, presence: true
+
+  def self.authenticate_with_credentials(email, password)
+    
+    email = email.downcase.strip
+    @user = User.find_by_email(email)
+
+
+    if @user && @user.authenticate(password)
+      return @user
+    else 
+      return nil
+    end
+  end
+
+
 end
--- spec/user_spec.rb
+++ spec/user_spec.rb
@@ -2,11 +2,216 @@ RSpec.describe User, type: :model do
 
   describe 'Validations' do
     # validation examples here
+    it 'is valid' do
+
+      test1 = User.create(
+        name:'rando', 
+        first_name:'rando', 
+        last_name:'rando',
+        email:'rando@gmail.com',
+        password:'123123' ,
+        password_confirmation:'123123'
+      )
+      puts test1.errors.full_messages
+      puts 'valid registration'
+      expect(test1).to be_valid
+      
+    end
     
+    it 'should fail because user did not enter first name' do
+
+      test1 = User.create(
+        name:'rando',
+        first_name:nil,
+        last_name:'rando',
+        email:'rando@gmail.com',
+        password:'123123' ,
+        password_confirmation:'123123'
+      )
+      puts test1.errors.full_messages
+      expect(test1.valid?).to be false
+      
+    end
+
+    it 'should fail because user did not enter last name' do
+
+      test1 = User.create(
+        name:'rando', 
+        first_name:'rando',
+        last_name:nil,
+        email:'rando@gmail.com',
+        password:'123123' ,
+        password_confirmation:'123123'
+      )
+      puts test1.errors.full_messages
+      expect(test1.valid?).to be false
+      
+    end
+
+    it 'should fail because user did not an email' do
+
+      test1 = User.create(
+        name:'rando', 
+        first_name:'rando',
+        last_name:'rando',
+        email:nil,
+        password:'123123' ,
+        password_confirmation:'123123'
+      )
+      puts test1.errors.full_messages
+      expect(test1.valid?).to be false
+      
+    end
+    
+    it 'should fail because password dont match' do
+
+      test1 = User.create(
+        name:'rando', 
+        first_name:'rando',
+        last_name:'rando',
+        email:nil,
+        password:'123123' ,
+        password_confirmation:'1223'
+      )
+      puts test1.errors.full_messages
+      expect(test1.valid?).to be false
+      
+    end
+
+
+    it 'should fail second user registration due to two identical email' do
+
+      test1 = User.create(
+        name:'rando',
+        first_name:'rando',
+        last_name:'rando',
+        email:'rando@gmail.com',
+        password:'123123' ,
+        password_confirmation:'123123'
+      )
+      puts test1.errors.full_messages
+      expect(test1).to be_valid
+      
+      test2 = User.create(
+        name:'rando',
+        first_name:'rando',
+        last_name:'rando',
+        email:'RANDO@GMAIL.COM',
+        password:'123123',
+        password_confirmation:'123123'
+      )
+      puts test2.errors.full_messages
+      expect(test2.valid?).to be false
+    end
+
+    it 'should fail because user password is less than minimal of 6 character' do
+  
+      test1 = User.create(
+        name:'rando',
+        first_name:'rando',
+        last_name:'rando',
+        email:'RANDO@GMAIL.COM',
+        password:'124',
+        password_confirmation:'124'
+      )
+      puts test1.errors.full_messages
+      expect(test1.valid?).to be false
+    end
+
   end
 
   describe '.authenticate_with_credentials' do
     # examples for this class method here
+
+    it 'should be valid with credentials' do
+  
+      user1 = User.create(
+        name:'rando',
+        first_name:'rando',
+        last_name:'rando',
+        email:'rando@gmail.com',
+        password:'1234567',
+        password_confirmation:'1234567'
+      )
+
+
+     user1 = User.authenticate_with_credentials('rando@gmail.com', '1234567')
+     puts user1.errors.full_messages
+      expect(user1).not_to be(nil)
+    end
+
+
+    it 'should fail becaues password does not match' do
+  
+      user1 = User.create(
+        name:'rando',
+        first_name:'rando',
+        last_name:'rando',
+        email:'rando@gmail.com',
+        password:'1234567',
+        password_confirmation:'1234567'
+      )
+
+
+     user1 = User.authenticate_with_credentials('rando@gmail.com', '12367')
+      expect(user1).to be(nil)
+    end
+
+    it 'should fail becaues email does not match' do
+  
+      user1 = User.create(
+        name:'rando',
+        first_name:'rando',
+        last_name:'rando',
+        email:'rando@gmail.com',
+        password:'1234567',
+        password_confirmation:'1234567'
+      )
+
+
+     user1 = User.authenticate_with_credentials('rdo@gmail.com', '1234567')
+     
+      expect(user1).to be(nil)
+    end
+
+
+    it 'should be valid with upper case email' do
+  
+      user1 = User.create(
+        name:'rando',
+        first_name:'rando',
+        last_name:'rando',
+        email:'rando@gmail.com',
+        password:'1234567',
+        password_confirmation:'1234567'
+      )
+
+
+     user1 = User.authenticate_with_credentials('    RANdo@gMail.com', '1234567')
+     
+      expect(user1).not_to be(nil)
+    end
+
+
+    it 'should fail becaues password does not match' do
+  
+      user1 = User.create(
+        name:'rando',
+        first_name:'rando',
+        last_name:'rando',
+        email:'rando@gmail.com',
+        password:'PASSWORD',
+        password_confirmation:'PASSWORD'
+      )
+
+
+     user1 = User.authenticate_with_credentials('rdo@gmail.com', 'password')
+     
+      expect(user1).to be(nil)
+    end
+
+
+
   end
 
 end
